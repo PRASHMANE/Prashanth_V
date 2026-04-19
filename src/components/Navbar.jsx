bdfa9a08b3e7b2d3adb8d805a9f5c2231b09b9be
PRASHMANE <prashanthvprashanthv46@gmail.com>
2026-04-20 02:57:06 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,30 @@
+import { useState } from "react";
 import "./Navbar.css";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
   const links = [
-    "Home",
-    "About",
-    "Skills",
-    "Experience",
-    "Projects",
-    "Education",
-    "Achievements",
-    "Contact",
+    "Home","About","Skills","Experience",
+    "Projects","Education","Achievements","Contact",
   ];
 
   return (
     <div className="navbar">
       <h2 className="logo">Prashanth.V</h2>
 
-      <div className="nav-links">
+      {/* 🔥 Hamburger */}
+      <div className="hamburger" onClick={() => setOpen(!open)}>
+        ☰
+      </div>
+
+      <div className={`nav-links ${open ? "show" : ""}`}>
         {links.map((link, index) => (
           <a
             key={index}
             href={`#${link.toLowerCase()}`}
             className="nav-item"
+            onClick={() => setOpen(false)}
           >
             {link}
           </a>
